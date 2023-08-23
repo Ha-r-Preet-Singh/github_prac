839529d7e4c83dc21a74a14b60df5dce451a7689
Harpreet Singh <harpreetsingh70159@gmail.com>
2023-08-24 01:30:36 +0530

    Created Second Page

---
+++ lib/second_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class NextPage extends StatefulWidget {
+  const NextPage({super.key});
+
+  @override
+  State<NextPage> createState() => _NextPageState();
+}
+
+class _NextPageState extends State<NextPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          height: 200,
+          width: 200,
+          decoration: BoxDecoration(
+            color: Colors.yellow,
+          ),
+        ),
+      ),
+    );
+  }
+}
